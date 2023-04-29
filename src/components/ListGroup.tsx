45c71bc3a7bc730cc12f3de26327db56418d076a
unknown <gideonkiprono6369@gmail.com>
2023-04-29 23:46:50 +0300

    Project Added

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,21 @@
+import { MouseEvent } from "react";
+
+function ListGroup() {
+  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  //Event Handler
+  const handleClick = (event: MouseEvent) => console.log(event);
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((items, index) => (
+          <li className="list-group-item" key={items} onClick={handleClick}>
+            {items}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+export default ListGroup;
