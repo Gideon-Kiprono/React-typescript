8f1c1447bf3dc52150cf2fd868edf654520ee158
Gideon Kiprono <gideonkiprono6369@gmail.com>
2023-05-12 23:07:41 +0300

    Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import Alert from "./components/Alert";
+import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
 
 function App() {
+  const handleClick = () => {
+    console.log("Button Clicked");
+  };
   // const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
   // const handleSelectItem = (item: String) => {
@@ -11,14 +15,15 @@ function App() {
   return (
     <div>
       {
-        /* <ListGroup
+        /* /* <ListGroup
         item={items}
         heading="Cities"
         onSelectItem={ handleSelectItem}
       /> */
-        <Alert>
-          Hello<span>World</span>{" "}
-        </Alert>
+        // <Alert>
+        //   Hello<span>World</span>{" "}
+        // </Alert> */}
+        <Button onClick={handleClick}>Click Me</Button>
       }
     </div>
   );
---
+++ src/components/Button.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+interface Props {
+  onClick: () => void;
+  children: React.ReactNode;
+}
+
+const Button: React.FC<Props> = ({ onClick, children }) => {
+  return <button onClick={onClick}>{children}</button>;
+};
+
+export default Button;
